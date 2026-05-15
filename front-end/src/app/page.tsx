7b87ec26e962cd960a79d9d0f9cb77872ff83104
Muhammad Nasiru <nasaminu13@gmail.com>
2026-05-15 21:06:44 +0100

    feat: Navigation bar in layout of project and Nav component

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -8,22 +8,6 @@ export default function HomePage() {
   return (
     <>
       <div className="min-h-screen bg-gradient-to-br from-slate-50 to-white text-gray-800">
-        <nav className="flex justify-between items-center px-8 py-5">
-          <Link href="/" className="text-2xl font-bold tracking-tight">
-            <h1 className="text-2xl font-bold tracking-tight">HireLink</h1>
-          </Link>
-
-          <div className="space-x-6 hidden md:flex">
-            <Link href="/jobs">Jobs</Link>
-            <Link href="/companies">Companies</Link>
-            <Link href="/about">About</Link>
-          </div>
-
-          <button className="bg-black text-white px-4 py-2 rounded-xl">
-            <Link href="/register">Sign Up</Link>
-          </button>
-        </nav>
-
         <section className="px-8 py-16">
           <h2 className="text-4xl font-bold mb-4">
             Find Work That Fits Your Life
@@ -57,7 +41,7 @@ export default function HomePage() {
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6 }}
-          className="bg-gray-100 rounded-3xl h-[300px] mt-10 flex items-center justify-center"
+          className="bg-gray-100 rounded-3xl h-[75] mt-10 flex items-center justify-center"
         >
           Illustration
         </motion.div>
